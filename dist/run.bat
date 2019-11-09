6833a3a64c60063773b31408a1bdb50615cf4fa0
Dennis Schwarz <McIntozh@gmx.net>
2019-11-09 13:47:59 +0100

    Fixed some small issues and added a dist-folder

---
+++ dist/run.bat
@@ -0,0 +1,1 @@
+java -jar %cd%\HedgeVoiceMapper.jar
--- src/main/java/de/schwarzdennis/hvm/logic/Properties.java
+++ src/main/java/de/schwarzdennis/hvm/logic/Properties.java
@@ -4,17 +4,27 @@ import java.io.File;
 
 public class Properties {
 
-    public File getDefaultVoicePath() {
-        java.util.Properties sysprops = System.getProperties();
-        String fileSeperator = sysprops.getProperty("file.separator");
-        String prefix = sysprops.getProperty("user.home");
+  public File getDefaultVoicePath() {
+    java.util.Properties sysprops = System.getProperties();
+    String fileSeperator = sysprops.getProperty("file.separator");
+    String prefix = sysprops.getProperty("user.home");
 
-        if ("Linux".equalsIgnoreCase(sysprops.getProperty("os.name"))) {
-            if (prefix != null) {
-                return new File(prefix + fileSeperator + ".hedgewars" + fileSeperator + "Data" + fileSeperator + "Sounds" + fileSeperator + "voices");
-            }
-        }
-
-        return null;
+    if ("Linux".equalsIgnoreCase(sysprops.getProperty("os.name"))) {
+      if (prefix != null) {
+        return new File(prefix + fileSeperator + ".hedgewars" + fileSeperator + "Data" + fileSeperator + "Sounds" + fileSeperator + "voices");
+      }
     }
+    if ("Windows".equalsIgnoreCase(sysprops.getProperty("os.name"))) {
+      if (prefix != null) {
+        return new File(prefix + fileSeperator + "Hedgewars" + fileSeperator + "Data" + fileSeperator + "Sounds" + fileSeperator + "voices");
+      }
+    }
+    if ("MacOS".equalsIgnoreCase(sysprops.getProperty("os.name"))) {
+      if (prefix != null) {
+        return new File(prefix + fileSeperator + "Library/Application Support/Hedgewars" + fileSeperator + "Data" + fileSeperator + "Sounds" + fileSeperator + "voices");
+      }
+    }
+
+    return null;
+  }
 }
